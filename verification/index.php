37c2aa98a1666f650a8148dd4f34828f5035efc4
74WINXD <ccw.la@outlook.com>
2021-03-09 12:27:19 +0100

    Ik heb een nieuwe php file aangemaakt

---
+++ verification/index.php
@@ -0,0 +1,29 @@
+<?php
+// PHP code goes here
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="CSS/style.css">
+</head>
+<body>
+    <header>
+        <p>Theater zuidplein</p>
+    </header>
+    <main>
+        <form action="">
+            <input type="text" id="code" name="code" value=""><br>
+            <input type="submit" value="Submit">
+        </form>
+    </main>
+    <footer>
+
+    </footer>
+</body>
+</html>
