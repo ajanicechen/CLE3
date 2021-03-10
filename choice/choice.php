c8dfc6032f1d304e2d8eaa70fb85ef79813c77f8
Annie <a.janice.chen@gmail.com>
2021-03-10 11:57:29 +0100

    styled choice.php

--- choice/choice.php
+++ choice/choice.php
@@ -9,21 +9,18 @@
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
-    <link rel="stylesheet" href="../verification/CSS/style.css">
+    <link rel="stylesheet" href="../choice/CSS/style.css">
 </head>
 <body>
     <header>
-        <h1>What should main character do??</h1>
+        <h1>What should Main Character do??</h1>
     </header>
     <main>
         <form action="">
-            <button class="buttons">Choice A</button>
-            <button class="buttons">Choice B</button>
+            <button class="buttons A">Choice A</button>
+            <button class="buttons B">Choice B</button>
             <input type="submit" value="Submit">
         </form>
     </main>
-    <footer>
-
-    </footer>
 </body>
 </html>
