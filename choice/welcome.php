0fbd85fa7a066001aa6761038c198ca974355bdc
Annie <a.janice.chen@gmail.com>
2021-03-10 13:04:51 +0100

    welcome page

---
+++ choice/welcome.php
@@ -0,0 +1,23 @@
+<?php
+?>
+
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="../choice/CSS/style.css">
+</head>
+<body>
+    <main>
+        <div class="welcome">
+            <h2>Welcome to Gintama Live Action!<br>
+            Play will start soon,<br>
+            please wait...
+        </div>
+    </main>
+</body>
+</html>
