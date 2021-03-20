b7e24250d6860fc70670e65bfdab4e7ca0fc1f50
74WINXD <ccw.la@outlook.com>
2021-03-20 21:42:08 +0100

    Ik heb de navbar aangepast en de autoscroll.js een smooth scroll gegeven.

--- onePager/javascript/autoscroll.js
+++ onePager/javascript/autoscroll.js
@@ -3,26 +3,26 @@ const storyButton = document.querySelector("#story")
 const storySection = document.querySelector("#storysection")
 
 storyButton.addEventListener("click", function (){
-    storySection.scrollIntoView();
+    storySection.scrollIntoView({behavior:'smooth'});
 })
 
 const deelButton = document.querySelector("#deel")
 const deelSection = document.querySelector("#deelsection")
 
 deelButton.addEventListener("click", function (){
-    deelSection.scrollIntoView();
+    deelSection.scrollIntoView({behavior:'smooth'});
 })
 
 const overButton = document.querySelector("#over")
 const overSection = document.querySelector("#oversection")
 
-    overButton.addEventListener("click", function (){
-    overSection.scrollIntoView();
+overButton.addEventListener("click", function (){
+    overSection.scrollIntoView({behavior:'smooth'});
 })
 
 const steunButton = document.querySelector("#steun")
 const steunSection = document.querySelector("#steunsection")
 
 steunButton.addEventListener("click", function (){
-    steunSection.scrollIntoView();
+    steunSection.scrollIntoView({behavior:'smooth'});
 })
